5ae885a466b864d666d8c291a718266cceda31da
reviaditiaardianputra-netizen <reviaditiaardianputra@gmail.com>
2026-09-15 14:03:27 +0700

    GameCharacter.cs

--- GameEnkapProj2/GameEnkapsulasi/GameCharacter.cs
+++ GameEnkapProj2/GameEnkapsulasi/GameCharacter.cs
@@ -8,15 +8,12 @@ namespace GameEncapsulationDemo
 {
     Console.WriteLine("=== DEMO KONSTRUKTOR ===\n");
 
-    // 1. Instansiasi menggunakan konstruktor Default
     Character hero1 = new Character();
     hero1.ShowStats();
 
-    // 2. Instansiasi menggunakan konstruktor Berparameter (2 parameter)
     Character hero2 = new Character("C-002", "Liara");
     hero2.ShowStats();
 
-    // 3. Instansiasi menggunakan konstruktor Berparameter (3 parameter)
     Character hero3 = new Character("C-003", "Garrosh", "Orc Warrior");
     hero3.ShowStats();
 
